26c42914aad988326c7ccbe50a1d39e37cabf413
SapphireBeehive <118197084+SapphireBeehive@users.noreply.github.com>
2026-01-02 15:39:37 -0800

    fix(scripts): suppress shellcheck SC2034 for unused color variables
    Color variables are defined as a complete set for consistency and
    potential future use. Added disable comments for unused ones.

--- scripts/logs-report.sh
+++ scripts/logs-report.sh
@@ -17,13 +17,16 @@ if [[ -t 1 ]]; then
     RED='\033[0;31m'
     GREEN='\033[0;32m'
     YELLOW='\033[1;33m'
+    # shellcheck disable=SC2034
     BLUE='\033[0;34m'
     CYAN='\033[0;36m'
     NC='\033[0m'
 else
     RED=''
     GREEN=''
+    # shellcheck disable=SC2034
     YELLOW=''
+    # shellcheck disable=SC2034
     BLUE=''
     CYAN=''
     NC=''
